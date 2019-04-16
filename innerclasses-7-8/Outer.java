436fc3144deff33583da32c16299388a6f0dbf4f
Greg Birch <gregpbirch@gmail.com>
2019-04-16 07:50:24 +0100

    Create Outer.java

---
+++ innerclasses-7-8/Outer.java
@@ -0,0 +1,27 @@
+
+public class Outer {
+	
+	private String name = "Original Outer Name";
+	private void displayName() {
+		System.out.println(this.name);
+	}
+	
+	private Inner createInner() {
+		return new Inner();		
+	}
+	
+	
+	private class Inner {
+		private String innerName = "Inner Name";
+		private void changeOuter() {
+			Outer.this.name = "Altered Outer Name";
+		}
+	}
+	
+	public static void main(String[] args) {
+		Outer outer = new Outer();
+		outer.createInner().changeOuter();
+		outer.displayName();
+		System.out.println(outer.createInner().innerName);
+	}
+}
